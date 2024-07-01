83fcc138cb11dc2bd9ca89aab37b07964eac857e
jiasuan <38315168+jiasuan@users.noreply.github.com>
2024-07-01 22:15:54 +0800

    Update next.config.mjs
    Set default path

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
     env: {
         SERVER_ENDPOINT: "https://jsonplaceholder.typicode.com"
-    }
+    },
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/users',
+            permanent: true,
+          },
+        ]
+    },
 };
 
 export default nextConfig;
